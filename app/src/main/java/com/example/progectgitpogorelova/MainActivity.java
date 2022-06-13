eb518cb731e5ddd9e3c444bce625c521f9299ce5
kkskfi <dashapogorelova17@gmail.com>
2022-06-13 18:16:29 +0700

    коммит1 для сессион2

--- app/src/main/java/com/example/progectgitpogorelova/MainActivity.java
+++ app/src/main/java/com/example/progectgitpogorelova/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//коммит1 для сессион2
         super.onCreate(savedInstanceState);//создали ветку сессион2
         setContentView(R.layout.activity_main);//коммит1 для сессион1
     }
