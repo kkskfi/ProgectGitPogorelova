677cc6d5da0257e40b4438ae06c03c22a530dff2
kkskfi <dashapogorelova17@gmail.com>
2022-06-13 18:34:22 +0700

    коммит4 для мэйн

--- app/src/main/java/com/example/progectgitpogorelova/MainActivity.java
+++ app/src/main/java/com/example/progectgitpogorelova/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//коммит4 для мэйн
         super.onCreate(savedInstanceState);//коммит3 для мэйн
         setContentView(R.layout.activity_main);//коммит2 для мэйн
     }
